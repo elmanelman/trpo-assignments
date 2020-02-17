eb7ac5cf06343e3e594f8bee7ee8b09c9fd6c992
Elman <xineo2530@gmail.com>
2020-02-17 05:59:28 +0300

    Добавьте файлы проекта.

---
+++ AbstractFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
