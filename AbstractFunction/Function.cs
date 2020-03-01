c608745ea3a3d338c7203ca7c983e47b3782a3a7
Elman <xineo2530@gmail.com>
2020-03-01 23:47:44 +0300

    Implemented some polynomial functions.

---
+++ AbstractFunction/Function.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+
+namespace AbstractFunction
+{
+    /// <summary>
+    ///     Абстрактная функция.
+    /// </summary>
+    /// <remarks>
+    ///     Поддерживает вычисление значения в точке.
+    /// </remarks>
+    internal abstract class Function
+    {
+        public abstract double Evaluate(double x);
+    }
+
+    /// <summary>
+    ///     Одночлен от переменной x.
+    /// </summary>
+    internal class Monomial : Function
+    {
+        private double Scale { get; }
+        private uint Degree { get; }
+
+        public Monomial(double scale, uint degree)
+        {
+            Scale = scale;
+            Degree = degree;
+        }
+
+        public override double Evaluate(double x)
+        {
+            return Scale * Math.Pow(x, Degree);
+        }
+
+        public override string ToString()
+        {
+            return Degree switch
+            {
+                0 => $"{Scale}",
+                1 => $"({Scale} * x)",
+                _ => $"({Scale} * x^{Degree})"
+            };
+        }
+    }
+
+    /// <summary>
+    ///     Линейная функция от переменной x.
+    ///     Вид: a * x + b
+    /// </summary>
+    internal class Line : Function
+    {
+        private const int MonomialCount = 2;
+
+        public Monomial[] Monomials;
+
+        public Line(params double[] coefs)
+        {
+            Monomials = new Monomial[MonomialCount];
+
+            if (coefs.Length != MonomialCount)
+                throw new ArgumentException("invalid coefficient count");
+
+            for (var i = 0; i < Monomials.Length; ++i) Monomials[i] = new Monomial(coefs[i], (uint) i);
+        }
+
+        public override double Evaluate(double x)
+        {
+            return Monomials.Sum(monomial => monomial.Evaluate(x));
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" + ", Monomials.Select(monomial => monomial.ToString()));
+        }
+    }
+
+    /// <summary>
+    ///     Квадратичная функция от переменной x.
+    /// </summary>
+    /// <remarks>
+    ///     Вид: a * x^2 + b * x + c
+    /// </remarks>
+    internal class Quadratic : Function
+    {
+        private const int MonomialCount = 3;
+
+        public Monomial[] Monomials;
+
+        public Quadratic(params double[] coefs)
+        {
+            Monomials = new Monomial[MonomialCount];
+
+            if (coefs.Length != MonomialCount)
+                throw new ArgumentException("invalid coefficient count");
+
+            for (var i = 0; i < Monomials.Length; ++i) Monomials[i] = new Monomial(coefs[i], (uint) i);
+        }
+
+        public override double Evaluate(double x)
+        {
+            return Monomials.Sum(monomial => monomial.Evaluate(x));
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" + ", Monomials.Select(monomial => monomial.ToString()));
+        }
+    }
+
+    /// <summary>
+    ///     Кубическая функция от переменной x.
+    /// </summary>
+    /// <remarks>
+    ///     Вид: a * x^3 + b * x^2 + c * x + d
+    /// </remarks>
+    internal class Cubic : Function
+    {
+        private const int MonomialCount = 4;
+
+        public Monomial[] Monomials;
+
+        public Cubic(params double[] coefs)
+        {
+            Monomials = new Monomial[MonomialCount];
+
+            if (coefs.Length != MonomialCount)
+                throw new ArgumentException("invalid coefficient count");
+
+            for (var i = 0; i < Monomials.Length; ++i) Monomials[i] = new Monomial(coefs[i], (uint) i);
+        }
+
+        public override double Evaluate(double x)
+        {
+            return Monomials.Sum(monomial => monomial.Evaluate(x));
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" + ", Monomials.Select(monomial => monomial.ToString()));
+        }
+    }
+}
--- AbstractFunction/Program.cs
+++ AbstractFunction/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace AbstractFunction
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///     Точка входа в приложение.
+        /// </summary>
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var functions = ReadFunctionsFromFile();
+
+                foreach (var function in functions) Console.WriteLine(function.ToString());
+            }
+            catch (Exception e)
+            {
+                // debug
+                Console.WriteLine(e.GetType() + " " + e.Message);
+            }
+        }
+
+        private static IEnumerable<Function> ReadFunctionsFromFile()
+        {
+            IFormatProvider doublesFormatter = new NumberFormatInfo {NumberDecimalSeparator = "."};
+
+            var functionCount = 0;
+
+            using var sr = new StreamReader("input.txt", Encoding.Default);
+            string line;
+
+            if ((line = sr.ReadLine()) != null) functionCount = int.Parse(line);
+
+            var functions = new List<Function>();
+
+            for (var i = 0; i < functionCount; i++)
+                if ((line = sr.ReadLine()) != null)
+                {
+                    Function f;
+
+                    var tokens = line.Split(" ");
+                    var functionType = tokens[0];
+                    var coefs = tokens
+                        .Skip(1)
+                        .Select(token => double.Parse(token, doublesFormatter))
+                        .ToArray();
+
+                    switch (functionType)
+                    {
+                        case "line":
+                            f = new Line(coefs);
+                            functions.Add(f);
+                            break;
+                        case "quadratic":
+                            f = new Quadratic(coefs);
+                            functions.Add(f);
+                            break;
+                        case "cubic":
+                            f = new Cubic(coefs);
+                            functions.Add(f);
+                            break;
+                        default:
+                            throw new FormatException("invalid function type: " + tokens[0]);
+                    }
+                }
+
+            return functions;
         }
     }
 }
