56ad6e344d92b70351ce577283b69c02f8dcb7c8
Elman Abdurahmanov <xineo2530@gmail.com>
2020-06-05 11:58:18 +0300

    Add files via upload

---
+++ client.server/Tcp.Server/EnteringPointServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SomeProject.Library.TcpServer;
+
+namespace SomeProject.TcpServer
+{
+    internal class EnteringPointServer
+    {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        private static void Main()
+        {
+           try
+           {
+               var server = new Server();
+
+               server.StartListener().Wait();
+           }
+           catch(Exception e)
+           {
+               Console.WriteLine(e.Message);
+           }
+        }
+    }
+}
